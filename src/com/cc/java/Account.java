dda3b676153a019799765620956955876c4ef32d
DonCerebro1 <85444482+DonCerebro1@users.noreply.github.com>
2021-12-02 15:52:51 +0100

    added Account Class and setter/getter

---
+++ src/com/cc/java/Account.java
@@ -0,0 +1,42 @@
+package com.cc.java;
+
+public class Account {
+    private String bankName;
+    private int currentBalance;
+    private int newBalance;
+
+    
+    public Account(int currentBalance, String bankName) {
+        this.currentBalance = currentBalance;
+        this.bankName = bankName;
+    }
+
+    public void getBankInformation(){
+        System.out.println(bankName + " der aktuelle Kontostand beträgt: " + currentBalance);
+
+    }
+
+    public int getCurrentBalance() {
+        return currentBalance;
+    }
+
+    public void setCurrentBalance(int currentBalance) {
+        this.currentBalance = currentBalance;
+    }
+
+    public int getNewBalance() {
+        return newBalance;
+    }
+
+    public void setNewBalance(int newBalance) {
+        this.newBalance = currentBalance;
+        currentBalance = currentBalance + newBalance;
+    }
+
+    
+
+    
+
+    
+
+}
--- src/com/cc/java/App.java
+++ src/com/cc/java/App.java
@@ -1,10 +1,35 @@
 package com.cc.java;
 
+import java.io.Console;
+
 public class App {
     
     public static void main(String[] args) {
+        Account bankAcc = new Account(10000, "Konto  ");
+        Account bankAccTwo = new Account(10000, "Konto 2");
+        Account bankAccThree = new Account(10000,"Konto 3");
 
-    
+        bankAcc.getBankInformation();
+        bankAccTwo.getBankInformation();
+        bankAccThree.getBankInformation();
+
+        output("-----------------------------");
+
+        bankAcc.setNewBalance(10000);
+        bankAccTwo.setNewBalance(20000);
+        bankAccThree.setNewBalance(100000);
+
+        bankAcc.getBankInformation();
+        bankAccTwo.getBankInformation();
+        bankAccThree.getBankInformation();
+
     }
 
+
+
+
+
+    static void output(String outputStr){
+        System.out.println(outputStr);
+    }
 }
